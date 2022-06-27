e34959b50fd3ace25a4c326ef7fbc976475b7c9a
julian-uribe47 <juribedio@hotmail.com>
2022-06-27 17:02:15 -0500

    formularios mercadoLiebre

--- app.js
+++ app.js
@@ -9,3 +9,9 @@ app.listen(3030, ()=> console.log('ejecutando servidor'));
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './views/home.html'))
 });
+app.get('/crearCuenta', (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/crearCuenta.html'))
+});
+app.get('/ingresar', (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/ingresar.html'))
+});
