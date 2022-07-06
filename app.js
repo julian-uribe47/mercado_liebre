846555cee406eb69973f6685db2af8f03700ec69
julian-uribe47 <juribedio@hotmail.com>
2022-07-05 19:28:21 -0500

    configuracion heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 
-app.listen(3030, ()=> console.log('ejecutando servidor'));
+app.listen(process.env.PORT || 3000, ()=> {console.log("ejecutando servidor")});
 
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, './views/home.html'))
